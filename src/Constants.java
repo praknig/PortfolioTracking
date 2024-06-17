00cb331d17681eb676233ae39b436c9bc31327f0
Prakhar <5721698+praknig@users.noreply.github.com>
2024-06-17 23:31:40 +0530

    changed zipped logic

--- src/Constants.java
+++ src/Constants.java
@@ -1,8 +1,8 @@
 public class Constants {
-    public static int COMPANIES = 3000;
-    public static int PERCISION = 3;
-    public static int WRITE_LINE_LIMIT = 2000;
-    public static int WRITE_FILE_LIMIT = 30;
+    public static short COMPANIES = 3000;
+    public static short PERCISION = 3;
+    public static short WRITE_LINE_LIMIT = 2000;
+    public static short WRITE_FILE_LIMIT = 30;
     public static int ZIP_FILE_DELAY = 1000*60*30;
     public static int START = 0;
     public static int END = 4;
--- src/PortfolioBuilder.java
+++ src/PortfolioBuilder.java
@@ -1,23 +1,25 @@
 import java.io.*;
 
 import java.nio.file.Files;
-import java.nio.file.Paths;
+
 import java.nio.file.StandardOpenOption;
+import java.util.Random;
 
 public class PortfolioBuilder implements Runnable {
 
     private StringBuilder foliosData;
     private String fileName;
-    private int numWrites;
-    private int writeLimit;
+    private short numWrites;
+    private short writeLimit;
     private int start;
     private int end;
-    private int precision;
-    private int companies;
+    private short precision;
+    private short companies;
     private String workingFolder;
-    private int folioSizeLimit;
-    private int folioSize;
+    private short folioSizeLimit;
+    private short folioSize;
     private String configFile;
+    private short relevantLength;
 
     public PortfolioBuilder(int start, int end) {
         this.start = start;
@@ -27,10 +29,11 @@ public class PortfolioBuilder implements Runnable {
         this.fileName = "portfolios.txt";
         this.configFile = "portfolio_config.txt";
         this.companies = Constants.COMPANIES;
-        this.precision =  Constants.PERCISION;
+        this.precision = Constants.PERCISION;
         this.writeLimit = Constants.WRITE_FILE_LIMIT;
         this.folioSizeLimit = Constants.WRITE_LINE_LIMIT;
         this.foliosData = new StringBuilder();
+        this.relevantLength = 0;
     }
 
     public void run() {
@@ -53,8 +56,15 @@ public class PortfolioBuilder implements Runnable {
         catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        runTheShow();
+        int nums[] = new int[this.companies];
+        Random rand = new Random();
+        for(int i = 0 ;i<this.companies;i++)
+        {
+            nums[i] = Math.abs(rand.nextInt())%10;
+        }
+        this.printArray(nums);
+        System.out.println(buildString(nums));
+        //runTheShow();
     }
 
     private void makeCarryAdjustments(int[] companyArray, int limit) {
@@ -121,14 +131,71 @@ public class PortfolioBuilder implements Runnable {
         fl.renameTo(newFile);
 
     }
+    private StringBuilder buildString(int[] numArray)
+    {
+        StringBuilder strBuilder = new StringBuilder();
+        short startIndex = 1;
+        int lastIndex = numArray.length-1;
+        short countSameElement = 0;
+        int element=0;
+        strBuilder.append(numArray[0]).append("|");
+        for(short index = startIndex;index<lastIndex;index++)
+        {
+            if( (index+1)<lastIndex && numArray[index] == numArray[index+1] )
+            {
+                countSameElement++;
+                element = numArray[index];
+
+            }
+            else{
+                if(countSameElement>0)
+                {
+                    strBuilder.append("(").append( element).append( ",").append( countSameElement).append(")|");
+                    countSameElement = 0;
+                }
+                strBuilder.append(numArray[index]).append("|");
+            }
+        }
+        strBuilder.append(numArray[lastIndex]).append(System.lineSeparator());
+        return strBuilder;
+
+
+    }
     private void writeToFile(int[] numArray) {
 
+        /*short countZeros=0;
+        String s = "";
         for (int i = 0; i < this.companies; i++) {
 
-            this.foliosData.append(numArray[i]).append("|");
+            if(numArray[i] == 0)
+            {
+                countZeros++;
+            }
+            else {
+
+                if(countZeros>0)
+                {
+
+                    this.foliosData.append("["+countZeros+"]").append("|");
+                    s += "["+countZeros+"]"+"|";
+                    countZeros= 0;
+                }
+                s += numArray[i] + "|";
+                this.foliosData.append(numArray[i]).append("|");
+            }
+
+
+        }
+        if(countZeros>0)
+        {
+            this.foliosData.append("{"+countZeros+"}").append("|");
+            s += "{"+countZeros+"}|";
+
         }
         this.foliosData.deleteCharAt(this.foliosData.length()-1);
-        this.foliosData.append(System.lineSeparator());
+        //System.out.println("s---->"+s);
+        this.foliosData.append(System.lineSeparator()); */
+        this.foliosData.append(buildString(numArray));
         this.folioSize++;
         if (this.folioSize <= this.folioSizeLimit)
             return;
@@ -136,7 +203,7 @@ public class PortfolioBuilder implements Runnable {
         String foliosDataLocal = this.foliosData.toString();
         this.foliosData.setLength(0);
         String lastLine = this.returnLastLine(foliosDataLocal);
-        System.out.println("Thread=" + Thread.currentThread().getName() + "  " +lastLine);
+        System.out.println("Thread=" + Thread.currentThread().getName() + " " +lastLine);
         updateConfigFile(lastLine);
         this.folioSize = 0;
         File fl = new File(this.fileName);
@@ -156,8 +223,8 @@ public class PortfolioBuilder implements Runnable {
     }
 
     private void checkForTotal(int[] numArray, int limit) {
-        long sum = 0;
-        for (int i = 0; i < this.companies; i++) {
+        long sum = numArray[this.companies-1];
+        for (int i = 0; i < this.companies && i<=this.relevantLength; i++) {
             sum += numArray[i];
             if (sum > limit)
                 break;
@@ -167,20 +234,35 @@ public class PortfolioBuilder implements Runnable {
             writeToFile(numArray);
         }
     }
+    private void calculateRelevantLength(int companies[])
+    {
+        int lastIndex = this.companies-1;
+        short relIndex = 0;
+        for(short index = 0;index<lastIndex;index++)
+        {
+            if(companies[index]!= 0)
+            {
+                relIndex = index;
+            }
+        }
+        this.relevantLength = relIndex;
+    }
     private void runTheShow() {
 
         System.out.println("Thread Started " + Thread.currentThread().getName() + " start " + this.start + " end " + this.end);
-        int precision = this.precision;
+        short precision = this.precision;
         int companies = this.companies;
         int end = this.end;
         int lastIndex = companies - 1;
         int limit = (int) Math.pow(10, precision + 2);
         int[] companiesArray = loadInitialArray();
         printArray(companiesArray);
+        calculateRelevantLength(companiesArray);
         while (companiesArray[lastIndex] < end) {
             companiesArray[0]++;
             if (companiesArray[0] > limit) {
                 makeCarryAdjustments(companiesArray, limit);
+                calculateRelevantLength(companiesArray);
             }
             checkForTotal(companiesArray, limit);
         }
@@ -199,7 +281,7 @@ public class PortfolioBuilder implements Runnable {
             try {
                 FileReader fr = new FileReader(portfolioFile.getAbsolutePath());
                 BufferedReader bufferedReader = new BufferedReader(fr);
-                String line =  bufferedReader.readLine();
+                String line = bufferedReader.readLine();
                 bufferedReader.close();
                 fr.close();
                 if (line == null || line.length() == 0) {
@@ -209,7 +291,7 @@ public class PortfolioBuilder implements Runnable {
                     String percentages[] = line.split("\\|");
                     for (String percentage : percentages) {
                         percentage = percentage.trim();
-                        if(percentage == null  || percentage.length() == 0)
+                        if(percentage == null || percentage.length() == 0)
                         {
                             companiesArray[index] = 0;
                         }
